d919388eb1b1f871f11131f6740da1a2941fd18b
Patrick Dukes <pdukes3@gmail.com>
2024-05-30 14:04:28 -0400

    updates the preprocess and generation code

--- server/app.py
+++ server/app.py
@@ -1,9 +1,14 @@
 import os
 import random
 import pickle
-import xml.etree.ElementTree as ET
 import json
-from preprocess import preprocess_svgs  # Import the preprocess function
+import logging
+from lxml import etree as ET
+from preprocess import preprocess_svgs
+
+# Set up logging
+logging.basicConfig(level=logging.ERROR,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Set the working directory to the script's directory
 script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -11,11 +16,29 @@ os.chdir(script_dir)
 
 # Check if cache exists, if not run preprocessing
 if not os.path.exists('svg_cache.pkl'):
+    logging.info("svg_cache.pkl not found. Running preprocessing...")
     preprocess_svgs()
 
 # Load the SVG cache
 with open('svg_cache.pkl', 'rb') as cache_file:
     svg_cache = pickle.load(cache_file)
+
+# Convert string representations back to ElementTree objects
+for key, value in svg_cache.items():
+    try:
+        tree = ET.ElementTree(ET.fromstring(value['tree']))
+        value['tree'] = tree
+        if value.get('skin') is not None:
+            value['skin'] = tree.find(f".//*[@id='{value['skin']}']")
+        value['accessories'] = [
+            tree.find(f".//*[@id='{acc}']") for acc in value.get('accessories', [])]
+        if value.get('hair') is not None:
+            value['hair'] = tree.find(f".//*[@id='{value['hair']}']")
+        if value.get('accessory') is not None:
+            value['accessory'] = tree.find(f".//*[@id='{value['accessory']}']")
+    except Exception as e:
+        logging.error(
+            "Error converting string to ElementTree for key %s: %s", key, e)
 
 # Load skin and hair color combinations from JSON file
 with open('skin_hair_combinations.json', 'r') as json_file:
@@ -23,58 +46,78 @@ with open('skin_hair_combinations.json', 'r') as json_file:
 
 
 def generate_svg_headshot(accessory_color):
-    # Randomly choose skin and hair color
-    skin_color, hair_color, _ = random.choice(skin_hair_combinations)
+    try:
+        # Randomly choose skin and hair color
+        skin_color, hair_color, description = random.choice(
+            skin_hair_combinations)
 
-    # Randomly choose body and hair SVG identifiers
-    body_id = random.choice(
-        [key for key in svg_cache.keys() if key.startswith('Body')])
-    hair_id = random.choice(
-        [key for key in svg_cache.keys() if key.startswith('Hair')])
+        # Randomly choose body and hair SVG identifiers
+        body_id = random.choice(
+            [key for key in svg_cache.keys() if key.startswith('Body')])
+        hair_id = random.choice(
+            [key for key in svg_cache.keys() if key.startswith('Hair')])
 
-    # Get the SVG elements from the cache
-    body_elements = svg_cache[body_id]
-    hair_elements = svg_cache[hair_id]
+        # Get the SVG elements from the cache
+        body_elements = svg_cache[body_id]
+        hair_elements = svg_cache[hair_id]
 
-    # Update skin and accessory colors in the body SVG
-    if body_elements['skin'] is not None:
-        body_elements['skin'].attrib['fill'] = skin_color
+        # Update skin and accessory colors in the body SVG
+        if body_elements.get('skin') is not None:
+            body_elements['skin'].attrib['fill'] = skin_color
+        else:
+            logging.warning("Skin element not found in body elements")
 
-       # Update accessory colors in the body SVG
-    for accessory in body_elements['accessories']:
-        accessory.attrib['fill'] = accessory_color
+        # Update accessory colors in the body SVG
+        for accessory in body_elements.get('accessories', []):
+            accessory.attrib['fill'] = accessory_color
 
-    # Update hair and accessory colors in the hair SVG
-    if hair_elements['hair'] is not None:
-        hair_elements['hair'].attrib['fill'] = hair_color
-    if hair_elements['accessory'] is not None:
-        hair_elements['accessory'].attrib['fill'] = accessory_color
+        # Update hair and accessory colors in the hair SVG
+        if hair_elements.get('hair') is not None:
+            hair_elements['hair'].attrib['fill'] = hair_color
+        else:
+            logging.warning("Hair element not found in hair elements")
 
-    # Combine body and hair SVGs
-    combined_svg = ET.Element('svg', xmlns="http://www.w3.org/2000/svg")
+        if hair_elements.get('accessory') is not None:
+            hair_elements['accessory'].attrib['fill'] = accessory_color
+        else:
+            logging.warning("Accessory element not found in hair elements")
 
-    for child in body_elements['tree'].getroot():
-        combined_svg.append(child)
+        # Create a combined SVG root element with proper namespace
+        combined_svg = ET.Element(
+            'svg', nsmap={None: "http://www.w3.org/2000/svg"})
 
-    for child in hair_elements['tree'].getroot():
-        combined_svg.append(child)
+        description = description.replace('skin', f'skin ({skin_color})').replace(
+            'hair', f'hair ({hair_color})')
+        generation_comment = ET.Comment(f'{body_id} x {hair_id}, {description} with {
+                                        accessory_color} accessory')
+        combined_svg.insert(0, generation_comment)
 
-    # Convert combined SVG to string
-    combined_svg_str = ET.tostring(combined_svg).decode('utf-8')
+        # Append body and hair elements to the combined SVG
+        for child in body_elements['tree'].getroot():
+            combined_svg.append(child)
 
-    return combined_svg_str
+        for child in hair_elements['tree'].getroot():
+            combined_svg.append(child)
+
+        # Convert combined SVG to string
+        combined_svg_str = ET.tostring(
+            combined_svg, pretty_print=True, encoding='unicode')
+
+        return combined_svg_str
+
+    except Exception as e:
+        logging.error("Error generating SVG headshot: %s", e)
+        return None
 
 
 if __name__ == '__main__':
-    # This section is for generating a single SVG without starting the server
-    generate_single_svg = True
+    accessory_color = '#4ec764'  # Example accessory color
+    svg_headshot = generate_svg_headshot(accessory_color)
 
-    if generate_single_svg:
-        accessory_color = '#4ec764'  # Example accessory color
-        svg_headshot = generate_svg_headshot(accessory_color)
-
+    if svg_headshot:
         # Save the SVG headshot to a file
         with open('headshot.svg', 'w', encoding='utf-8') as file:
             file.write(svg_headshot)
-
-        print("SVG headshot generated and saved to 'headshot.svg'")
+        logging.info("SVG headshot generated and saved to 'headshot.svg'")
+    else:
+        logging.error("Failed to generate SVG headshot")
--- server/preprocess.py
+++ server/preprocess.py
@@ -1,7 +1,7 @@
 import pickle
-import xml.etree.ElementTree as ET
 import glob
 import os
+from lxml import etree as ET
 
 # Set the working directory to the script's directory
 script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -11,47 +11,50 @@ os.chdir(script_dir)
 def preprocess_svgs(body_svgs_dir='body_svgs', hair_svgs_dir='hair_svgs'):
     svg_cache = {}
 
-    # Debug: Print current working directory
-    print("Current working directory:", os.getcwd())
-
     # Process body SVGs
     body_files = glob.glob(os.path.join(body_svgs_dir, 'Body*.svg'))
-    # Debug: Print the list of body files found
-    print("Body files found:", body_files)
-
     for body_file in body_files:
         tree = ET.parse(body_file)
         root = tree.getroot()
 
-        body_elements = {'tree': tree, 'skin': None, 'accessories': []}
+        body_elements = {'tree': ET.tostring(
+            root), 'skin': None, 'accessories': []}
+
+        # Inject a comment with the name of the file
+        comment = ET.Comment(f'This is a body SVG from file: {
+                             os.path.basename(body_file)}')
+        root.insert(0, comment)
 
         for elem in root.iter():
             if 'id' in elem.attrib:
                 if elem.attrib['id'] == 'Head':
-                    body_elements['skin'] = elem
+                    body_elements['skin'] = elem.attrib['id']
                 elif elem.attrib['id'] in ['Tie', 'TieKnot', 'Bowtie']:
-                    body_elements['accessories'].append(elem)
+                    body_elements['accessories'].append(elem.attrib['id'])
 
         file_name = os.path.splitext(os.path.basename(body_file))[0]
         svg_cache[file_name] = body_elements
 
     # Process hair SVGs
     hair_files = glob.glob(os.path.join(hair_svgs_dir, 'Hair*.svg'))
-    # Debug: Print the list of hair files found
-    print("Hair files found:", hair_files)
-
     for hair_file in hair_files:
         tree = ET.parse(hair_file)
         root = tree.getroot()
 
-        hair_elements = {'tree': tree, 'hair': None, 'accessory': None}
+        hair_elements = {'tree': ET.tostring(
+            root), 'hair': None, 'accessory': None}
+
+        # Inject a comment with the name of the file
+        comment = ET.Comment(f'This is a hair SVG from file: {
+                             os.path.basename(hair_file)}')
+        root.insert(0, comment)
 
         for elem in root.iter():
             if 'id' in elem.attrib:
                 if elem.attrib['id'] == 'hair':
-                    hair_elements['hair'] = elem
+                    hair_elements['hair'] = elem.attrib['id']
                 elif elem.attrib['id'] == 'accessory':
-                    hair_elements['accessory'] = elem
+                    hair_elements['accessory'] = elem.attrib['id']
 
         file_name = os.path.splitext(os.path.basename(hair_file))[0]
         svg_cache[file_name] = hair_elements
